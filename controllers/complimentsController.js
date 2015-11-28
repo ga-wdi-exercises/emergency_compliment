48021c64cfc7efe728eede08978d673f06e56a54
Jesse Shawl <jesse@jshawl.com>
2015-11-28 18:54:04 -0500

    add show route

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -1,5 +1,16 @@
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
 module.exports = {
   index: function(req,res){
-    res.render("compliments/index.hbs", {compliments: ['one','two']});
-  }
+    res.render("compliments/index.hbs", {compliments: compliments});
+  },
+  show: function(req, res){
+    res.render("compliments/show.hbs", {compliment: compliments[req.params.index]});
+  },
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')()
 var complimentsController = require("./controllers/complimentsController")
 
 app.get("/compliments", complimentsController.index)
+app.get("/compliments/:index", complimentsController.show)
 
 app.listen(3000, function(){
   console.log('app listening on http://localhost:3000/')
