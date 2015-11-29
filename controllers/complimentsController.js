816faa59cfa2079fc867adec873473e4b6bd2d3d
Jesse Shawl <jesse@jshawl.com>
2015-11-28 19:16:42 -0500

    render json

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -2,7 +2,14 @@ var Compliment = require("../models/compliment")
 
 var complimentsController = {
   index: function(req,res){
-    res.render("compliments/index.hbs", {compliments: Compliment.all()});
+    res.format({
+      html: function(){
+	res.render("compliments/index.hbs", {compliments: Compliment.all()});
+      },
+      json: function(){
+        res.json(Compliment.all()) 
+      }
+    })
   },
   show: function(req, res){
     res.render("compliments/show.hbs", {compliment: Compliment.find(req.params.index)});
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var app = require('express')()
 var Compliment = require("./models/compliment")
 var complimentsController = require("./controllers/complimentsController")
 
-app.get("/compliments", complimentsController.index)
+app.use("*.json",function (req, res, next) {
+  req.headers.accept = 'application/json';
+  next();
+});
+
+app.get("/compliments/?:format?", complimentsController.index)
 app.get("/compliments/:index", complimentsController.show)
 
 app.listen(3000, function(){
