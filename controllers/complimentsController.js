2b03b05b64b4d90655a26687660f1886da33b3ff
Nicholas Olds <nolds9@yahoo.com>
2015-11-30 17:35:45 -0500

    compliments: add edit, update solution

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -10,11 +10,26 @@ var complimentsController = {
       compliment: Compliment.find([req.params.id])
     });
   },
+  new: function(req, res){
+    res.render('compliments/new.hbs');
+  },
   create: function(req, res){
     // use model to create compliment
     var compliment = Compliment.create(req.body.compliment)
     // redirect to compliment
     res.redirect("/compliments")
+  },
+  edit: function(req, res){
+    res.render('compliments/edit.hbs', {
+      id: req.params.id,
+      compliment: Compliment.find([req.params.id])
+    });
+  },
+  update: function(req, res) {
+    var id = parseInt(req.params.id);
+    var updatedCompliment = Compliment.find(id)
+    updatedCompliment = req.body.compliment;
+    res.redirect("/compliments/" + id)
   }
 }
 
--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var complimentsController = require("./controllers/complimentsController");
+var methodOverride = require("method-override")
 
+app.use(methodOverride('_method'))
 app.use(bodyParser.json());//
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-//  root
-app.get("/", complimentsController.index);
-
 // index
 app.get("/compliments", complimentsController.index);
+
+// root
+app.get("/", function(req, res){
+  res.redirect("/compliments")
+});
 
 // create
 app.post("/compliments", complimentsController.create);
@@ -18,6 +22,16 @@ app.post("/compliments", complimentsController.create);
 // show
 app.get("/compliments/:id", complimentsController.show);
 
+// new
+app.get("/compliments/new", complimentsController.new);
+
+// edit
+app.get("/compliments/:id/edit", complimentsController.edit);
+
+// update
+app.put("/compliments/:id", complimentsController.update);
+
+// Run our app on Port 3000
 app.listen(3000, function(){
   console.log("Listening on http://localhost:3000/");
 })
