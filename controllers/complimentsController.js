73a6330d8a778abc154e87e081deb1233f5c6be4
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-12-01 10:54:50 -0500

    adds update method

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -24,6 +24,13 @@ var complimentsController = {
     res.render('compliments/edit.hbs',{
       compliment: Compliment.find([req.params.id])
     });
+  },
+  // update action code goes here...
+  update: function(req,res){
+    var compliment = Compliment.update(req.body.compliment)
+    // redirect to compliment
+    res.redirect("/compliments")
+    });
   }
  // update action code goes here...
 }
