9a26f8d9d3662f6a0b3d4daddea975cb9ccfa075
Eric Stennett <estennett@gmail.com>
2015-12-02 11:14:47 -0500

    fixed error in index.js edit route

--- index.js
+++ index.js
@@ -37,4 +37,4 @@ app.listen(3000, function(){
   console.log("Listening on http://localhost:3000/");
 })
 
-app.get("/compliments/edit", function(){complimentsController.edit};
+app.get("/compliments/edit", function(){complimentsController.edit});
