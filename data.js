88c6f5eae4aabc9ce423d249a4b6d928526d4a16
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-03-25 14:47:02 -0400

    Adds Changes for Git Teams Branch for Angular

---
+++ data.js
@@ -0,0 +1,113 @@
+var data  = [
+  {
+    "text": "Good effort!"
+  },
+  {
+    "text": "What a fine sweater!"
+  },
+  {
+    "text": "I appreciate all of your opinions."
+  },
+  {
+    "text": "I like your style."
+  },
+  {
+    "text": "Your T-shirt smells fresh."
+  },
+  {
+    "text": "I love what you've done with the place."
+  },
+  {
+    "text": "You are like a spring flower; beautiful and vivacious."
+  },
+  {
+    "text": "I am utterly disarmed by your wit."
+  },
+  {
+    "text": "I really enjoy the way you pronounce the word 'ruby'."
+  },
+  {
+    "text": "You complete me."
+  },
+  {
+    "text": "Well done!"
+  },
+  {
+    "text": "I like your Facebook status."
+  },
+  {
+    "text": "That looks nice on you."
+  },
+  {
+    "text": "I like those shoes more than mine."
+  },
+  {
+    "text": "Nice motor control!"
+  },
+  {
+    "text": "You have a good taste in websites."
+  },
+  {
+    "text": "Your mouse told me that you have very soft hands."
+  },
+  {
+    "text": "You are full of youth."
+  },
+  {
+    "text": "I like your jacket."
+  },
+  {
+    "text": "I like the way you move."
+  },
+  {
+    "text": "You have a good web-surfing stance."
+  },
+  {
+    "text": "You should be a poster child for poster children."
+  },
+  {
+    "text": "Nice manners!"
+  },
+  {
+    "text": "I appreciate you more than Santa appreciates chimney grease."
+  },
+  {
+    "text": "I wish I was your mirror."
+  },
+  {
+    "text": "I find you to be a fountain of inspiration."
+  },
+  {
+    "text": "You have perfect bone structure."
+  },
+  {
+    "text": "I disagree with anyone who disagrees with you."
+  },
+  {
+    "text": "Way to go!"
+  },
+  {
+    "text": "Have you been working out?"
+  },
+  {
+    "text": "With your creative wit, I'm sure you could come up with better compliments than me."
+  },
+  {
+    "text": "I like your socks."
+  },
+  {
+    "text": "You are so charming."
+  },
+  {
+    "text": "Your cooking reminds me of my mother's."
+  },
+  {
+    "text": "You're tremendous!"
+  },
+  {
+    "text": "You deserve a compliment!"
+  },
+  {
+    "text": "Hello, good looking."
+  }
+];
---
+++ readme.md
@@ -0,0 +1,13 @@
+# WDI Emergency Compliment- Git-Teams-Starter (Angular)
+
+## Mission…
+
+Create your own version of [Emergency Compliment](http://emergencycompliment.com/) using Angular!
+
+When a user visits the site, they should be greeted with a random compliment to cheer them up.
+
+We are "faking" a back-end by using `data.js` for our compliments.
+
+## Level 1: Generic Compliment
+
+When you visit the root `http://localhost:8080/` it should display a randomly chosen compliment.
