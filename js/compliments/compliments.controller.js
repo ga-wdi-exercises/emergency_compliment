87e24ec790f70a4686f8a7118551d992d13b0895
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-03-25 15:29:41 -0400

    Student 1: Changes compliments module name to emergencyComp

--- js/compliments/compliments.controller.js
+++ js/compliments/compliments.controller.js
@@ -1,7 +1,7 @@
 `use strict`;
 
 (function(){
-  var app = angular.module("compliments");
+  var app = angular.module("emergencyComp");
   app.controller("complimentsController", ComplimentsController);
 
   function ComplimentsController(){
