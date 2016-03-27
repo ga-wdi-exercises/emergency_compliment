486f4e162a2b69f91d57f1ba17c5466a93e8cbcc
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-03-27 18:27:46 -0400

    Student 2 PR and Merging Changes

--- data.js
+++ data.js
@@ -1,113 +1,39 @@
 var data  = [
-  {
-    "text": "Good effort!"
-  },
-  {
-    "text": "What a fine sweater!"
-  },
-  {
-    "text": "I appreciate all of your opinions."
-  },
-  {
-    "text": "I like your style."
-  },
-  {
-    "text": "Your T-shirt smells fresh."
-  },
-  {
-    "text": "I love what you've done with the place."
-  },
-  {
-    "text": "You are like a spring flower; beautiful and vivacious."
-  },
-  {
-    "text": "I am utterly disarmed by your wit."
-  },
-  {
-    "text": "I really enjoy the way you pronounce the word 'ruby'."
-  },
-  {
-    "text": "You complete me."
-  },
-  {
-    "text": "Well done!"
-  },
-  {
-    "text": "I like your Facebook status."
-  },
-  {
-    "text": "That looks nice on you."
-  },
-  {
-    "text": "I like those shoes more than mine."
-  },
-  {
-    "text": "Nice motor control!"
-  },
-  {
-    "text": "You have a good taste in websites."
-  },
-  {
-    "text": "Your mouse told me that you have very soft hands."
-  },
-  {
-    "text": "You are full of youth."
-  },
-  {
-    "text": "I like your jacket."
-  },
-  {
-    "text": "I like the way you move."
-  },
-  {
-    "text": "You have a good web-surfing stance."
-  },
-  {
-    "text": "You should be a poster child for poster children."
-  },
-  {
-    "text": "Nice manners!"
-  },
-  {
-    "text": "I appreciate you more than Santa appreciates chimney grease."
-  },
-  {
-    "text": "I wish I was your mirror."
-  },
-  {
-    "text": "I find you to be a fountain of inspiration."
-  },
-  {
-    "text": "You have perfect bone structure."
-  },
-  {
-    "text": "I disagree with anyone who disagrees with you."
-  },
-  {
-    "text": "Way to go!"
-  },
-  {
-    "text": "Have you been working out?"
-  },
-  {
-    "text": "With your creative wit, I'm sure you could come up with better compliments than me."
-  },
-  {
-    "text": "I like your socks."
-  },
-  {
-    "text": "You are so charming."
-  },
-  {
-    "text": "Your cooking reminds me of my mother's."
-  },
-  {
-    "text": "You're tremendous!"
-  },
-  {
-    "text": "You deserve a compliment!"
-  },
-  {
-    "text": "Hello, good looking."
-  }
+  "Good effort!",
+  "What a fine sweater!",
+  "I appreciate all of your opinions.",
+  "I like your style.",
+  "Your T-shirt smells fresh.",
+  "I love what you've done with the place.",
+  "You are like a spring flower; beautiful and vivacious.",
+  "I am utterly disarmed by your wit.",
+  "I really enjoy the way you pronounce the word 'ruby'.",
+  "You complete me.",
+  "Well done!",
+  "I like your Facebook status.",
+  "That looks nice on you.",
+  "I like those shoes more than mine.",
+  "Nice motor control!",
+  "You have a good taste in websites.",
+  "Your mouse told me that you have very soft hands.",
+  "You are full of youth.",
+  "I like your jacket.",
+  "I like the way you move.",
+  "You have a good web-surfing stance.",
+  "You should be a poster child for poster children.",
+  "Nice manners!",
+  "I appreciate you more than Santa appreciates chimney grease.",
+  "I wish I was your mirror.",
+  "I find you to be a fountain of inspiration.",
+  "You have perfect bone structure.",
+  "I disagree with anyone who disagrees with you.",
+  "Way to go!",
+  "Have you been working out?",
+  "With your creative wit, I'm sure you could come up with better compliments than me.",
+  "I like your socks.",
+  "You are so charming.",
+  "Your cooking reminds me of my mother's.",
+  "You're tremendous!",
+  "You deserve a compliment!",
+  "Hello, good looking."
 ];
--- readme.md
+++ readme.md
@@ -8,6 +8,14 @@ When a user visits the site, they should be greeted with a random compliment to 
 
 We are "faking" a back-end by using `data.js` for our compliments.
 
+## Set-Up:
+
+1. `git clone`
+2. `$ cd emergency_compliment`
+3. `$ git checkout git-teams-starter`
+3. `$ hs`
+4.  visit http://localhost:8080/ in your browser!
+
 ## Level 1: Generic Compliment
 
 When you visit the root `http://localhost:8080/` it should display a randomly chosen compliment.
