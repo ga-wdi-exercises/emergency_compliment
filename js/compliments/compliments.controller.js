3969e878c5ef7afb13258a9943df27c8a2096252
Ryan Krolick <rkrolick@gmail.com>
2016-03-30 15:12:58 -0400

    base compliments controller added

---
+++ js/compliments/compliments.controller.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function(){
+angular
+.module("compliments");
+.controller("complimentsController", ComplimentsController);
+
+function ComplimentsController(){
+  var vm = this;
+}
+
+})()
