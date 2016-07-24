472bd087f4b7a3ff1d94f2566308dd9f4cd94908
Daniela Noir <daniela@danielanoir.com>
2016-07-24 18:57:48 -0400

    finished

--- server.rb
+++ server.rb
@@ -2,23 +2,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-get '/:name' do
   compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
+    "Smile, your website is beautiful!",
+    "You've got friends!",
+    "Here's a hug! xoxo",
+    "At least your hair looks good",
+    "<3"
   ]
-  # compliments = compliments.sample
 
   colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
-get '/' do
-@compliments
-@colors
-  erb :index
+get ("/") do
+@compliment = compliments.sample
+@colors = colors.sample
+  erb :compliment
+end
+
+get ("/:name") do
+  @personalized = "Hello #{params[:name]}"
+  @compliment = compliments.sample
+  @color = colors.sample
+  erb :compliment
 end
 
 
+post ("/") do
+  compliments.push(params[:compliment])
 end
