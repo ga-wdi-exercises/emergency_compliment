6ebe4233d99148c68d81750cebea409e5cceff34
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-24 21:23:23 -0400

    completed part 2

---
+++ server.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+get '/:name' do
+  @name = params[:name]
+  @compliments = compliments
+  @background_color = colors
+  erb :compliment
+end
