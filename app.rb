5787fd968e89c562c5b3db9d46eafc94fc80c1f0
Rob Romaniw <robertromaniw@gmail.com>
2016-10-30 19:24:02 -0400

    Level 1-3 completed | Minimal styling so far

--- app.rb
+++ app.rb
@@ -10,10 +10,13 @@ compliments = [
   "You're the world's #0 programmer"
 ]
 
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 get '/' do
   @greeting_name = "Hey There!"
   @greeting_msg = "Here's a little something to cheer you up:"
   @compliment = compliments.sample
+  @color = colors.sample
   erb :index
 end
 
@@ -22,5 +25,11 @@ get '/:name' do
   @greeting_name = "Hey #{@name}!"
   @greeting_msg = "Here's a little something to cheer you up:"
   @compliment = compliments.sample
+  @color = colors.sample
   erb :index
+end
+
+post '/add' do
+  compliments << params[:compliment]
+  redirect '/'
 end
