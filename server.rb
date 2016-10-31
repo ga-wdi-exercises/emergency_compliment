57877caaec02737f55e3ec46e99a62a07041412e
robert <robert.k25@gmail.com>
2016-10-30 23:12:15 -0400

    adds colors and post route

--- server.rb
+++ server.rb
@@ -10,13 +10,21 @@ compliments = [
   "The Force is strong with you"
 ]
 
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 get '/' do
   @compliment = compliments.sample
+  @color = colors.sample
   erb :compliment
 end
 
 get '/:name' do
   @name = params[:name].to_s
   @compliment = compliments.sample
+  @color = colors.sample
   erb :compliment
+end
+
+post '/compliment/create' do
+  compliments << params[:new]
 end
