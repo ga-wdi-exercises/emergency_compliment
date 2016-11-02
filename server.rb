ae5c93e8be2993fdc417e8f518e72838bedb829b
Bradley Cravens <bradleycravens@gmail.com>
2016-11-02 09:49:30 -0400

    inital commit

---
+++ server.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+get '/' do
+  @hello = "Hi. "
+  @compliment = compliments.sample
+  @color = colors.sample
+  erb :compliment
+end
+
+post '/' do
+  compliments << params[:compliment]
+  redirect '/'
+end
+
+get '/:name' do
+  @hello = "Hi, #{params[:name]}. "
+  @compliment = compliments.sample
+  @color = colors.sample
+  erb :compliment
+end
