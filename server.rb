32d2458c5b77240695ccf9b076ef8815e9a851b9
Diana B. Gobar <dianabgobar@gmail.com>
2016-11-03 08:50:05 -0400

    emergency compliment

---
+++ server.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+ compliments = [
+   "Your instructors love you",
+   "High five = ^5",
+   "Is it Ruby Tuesday yet?",
+   "It's almost beer o'clock",
+   "The Force is strong with you"
+ ]
+
+ colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+ get '/' do
+   @compliment = compliments.sample
+   @color = colors.sample
+   erb :compliment
+ end
+
+ get '/:name' do
+   @greet = "Hi, #{params[:name]}. "
+   @compliment = compliments.sample
+   @color = colors.sample
+   erb :compliment
+ end
+
+ post '/' do
+   compliments << params[:compliment]
+   redirect '/'
+ end
