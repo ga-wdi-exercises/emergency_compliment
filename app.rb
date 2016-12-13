ad92c99ef2d9a2a783bacdc5dd3741a046805f1e
myztajay <myztajay@hotmail.com>
2016-12-12 23:04:10 -0500

    made random quote and color functionality

--- app.rb
+++ app.rb
@@ -1,7 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 get '/' do
-    
+  @compliment = compliments.sample
+  @bg = colors.sample
   erb :index
+end
+
+get '/:name' do
+
+  @compliment = "Hello #{params[:name]}  #{compliments.sample}"
+  @bg = colors.sample
+  erb :index
+  form
+end
+
+get '/create' do
+  erb :post
 end
